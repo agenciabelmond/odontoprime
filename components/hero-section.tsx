165d90d7ae2837b4adae1b25b4f759ff9e931455
camilatabordac <camilatabordach@gmail.com>
2026-04-23 11:26:23 -0300

    gramatica

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -25,7 +25,7 @@ export function HeroSection() {
             >
               {"Referência em "}
               <span className="text-[#F5C25C]">
-                Osdontologia
+                Odontologia
               </span>
               {" no Rio de Janeiro"}
             </h1>
